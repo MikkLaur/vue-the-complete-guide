b63d65e0edbe0f1bb218892c9623cff4c2971975
Mikk Laur <mikk.laur@bigbank.ee>
2022-10-26 08:10:39 +0300

    using watchers

--- basics-05-using-the-native-event-object/app.js
+++ basics-05-using-the-native-event-object/app.js
@@ -2,19 +2,51 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: ''
+      name: '',
+      lastName: '',
+      // fullname: ''
     };
   },
-  // computed properties will be updated on DOM only when a computed property is changed
-  computed: {
-    fullname() {
-      console.log('Running again...');
-      if (this.name === "") {
-        return '';
+  // similar to computed properties
+  watch: {
+    // Watcher could watcvh a counter for example and when it exceeds 50, it will be reset.
+    counter(value) {
+      if (value > 50) {
+        const that = this;
+        setTimeout(function() {
+          that.counter = 0;
+        }, 2000);
+        this.counter = 0;
       }
-      return this.name + " " + "Wannabe";
     }
+
+    // // if data.name is modified, a watcher name() will be triggered
+    // // name(newValue, oldValue) //old value is sent as well!
+    // name(value) {
+    //   if(value === '' ) {
+    //     this.fullname = '';
+    //   } else {
+    //     this.fullname = value + ' ' + this.lastName;
+    //   }
+    // },
+    // lastName(value) {
+    //   if(value === '' ) {
+    //     this.fullname = '';
+    //   } else {
+    //     this.fullname = this.name + ' ' + value;
+    //   }
+    // }
   },
+  // computed properties will be updated on DOM only when a computed property is changed
+  // computed: {
+  //   fullname() {
+  //     console.log('Running again...');
+  //     if (this.name === "") {
+  //       return '';
+  //     }
+  //     return this.name + " " + "Wannabe";
+  //   }
+  // },
   methods: {
     outputFullname() {
 
