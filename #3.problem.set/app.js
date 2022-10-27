95a0bf78bd0bd34d86652285b1fea2352dd0bcbb
Mikk Laur <mikk.laur@bigbank.ee>
2022-10-27 08:38:24 +0300

    progress on #3 problem set

--- #3.problem.set/app.js
+++ #3.problem.set/app.js
@@ -1,8 +1,32 @@
 const app = Vue.createApp({
     data() {
         return {
-            counter: 0
+            counter: 0,
+            timedOut: false,
+            result: 'Not there yet'
         };
+    },
+    watch: {
+        counter(value) {
+            if(!timedOut) {
+                this.timedOut = true;
+                const that = this;
+                setTimeout(function() {
+                    that.counter = 0;
+                    that.timedOut = false;
+                }, 5000);
+            }
+
+            if (value < 37) {
+                this.result = 'Not there yet';
+            }
+            else if (value === 37) {
+                this.result = value;
+            }
+            else {
+                this.result = 'Too much!';
+            }
+        }
     },
     methods: {
         add(num) {
