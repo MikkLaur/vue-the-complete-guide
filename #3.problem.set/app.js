13960b31729f2dc76911fc3852c10bcd59955976
Mikk Laur <mikk.laur@bigbank.ee>
2022-10-28 08:22:24 +0300

    #.problem.set use computed property

--- #3.problem.set/app.js
+++ #3.problem.set/app.js
@@ -3,11 +3,14 @@ const app = Vue.createApp({
         return {
             counter: 0,
             timedOut: false,
-            result: 'Not there yet'
         };
     },
     watch: {
-        counter(value) {
+        // It is much more efficient to run this watcher on result instead of counter.
+        // counter triggers watcher every time Add button is used. 
+        // Result triggers watcher only when computed result() changes 
+        result(value) {
+            console.log("Watcher running...");
             if(!this.timedOut) {
                 this.timedOut = true;
                 const that = this;
@@ -17,14 +20,26 @@ const app = Vue.createApp({
                 }, 5000);
             }
 
-            if (value < 37) {
-                this.result = 'Not there yet';
-            }
-            else if (value === 37) {
-                this.result = value;
+            // if (value < 37) {
+            //     this.result = 'Not there yet';
+            // }
+            // else if (value === 37) {
+            //     this.result = value;
+            // }
+            // else {
+            //     this.result = 'Too much!';
+            // }
+        }
+    },
+    computed: {
+        result() {
+            if (this.counter < 37) {
+                return 'Not there yet!';
+            } else if (this.counter === 37) {
+                return this.counter;
             }
             else {
-                this.result = 'Too much!';
+                return 'Too much!';
             }
         }
     },
